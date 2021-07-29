8625c7c2bae7f1e7e4f7bb25182baf3f9a0082e0
andysongsong <sxj930916@gmail.com>
2021-07-29 18:54:21 +0000

    final version

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+  const result = [];
+  for (const i in matrix[0]) {
+    const resultEl = [];
+    for (const element of matrix) {
+      resultEl.push(element[i]);
     }
-}
+    result.push(resultEl);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticaljoin = transpose(letters).map((ls) => ls.join(""));
+  for (c of verticaljoin) {
+    if (c.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
